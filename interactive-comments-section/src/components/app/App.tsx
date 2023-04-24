28dd2e1757023d638cc950220a91c3c1d01c71bf
hardygrunter <ih8tw8@gmail.com>
2023-04-24 19:54:05 +0300

    update App

--- interactive-comments-section/src/components/app/App.tsx
+++ interactive-comments-section/src/components/app/App.tsx
@@ -1,12 +1,29 @@
-import Layout from '../Layout/Layout';
-import Comment from '../Comment/Comment';
+import Layout from "../Layout/Layout";
+import { AnswerForm } from "../AnswerForm/AnswerForm";
+
+import data from "./data.json";
+import { userContext } from "../../contexts/userContext";
+import CommentsList from "../CommentsList/CommentsList";
+import { storeContext } from "../../contexts/storeContext";
+
+import store from '../../store';
+store.currentUser.load();
 
 const App = () => {
+  const onAnswer = (answer: string) => {
+    store.comments.addComment(answer);
+  };
+
   return (
     <Layout>
-      <Comment/>
+      <userContext.Provider value={data.currentUser}>
+        <storeContext.Provider value={store} >
+        <CommentsList />
+        <AnswerForm onAnswer={onAnswer} sendBtn="Send" />
+        </storeContext.Provider>
+      </userContext.Provider>
     </Layout>
-  )
-}
+  );
+};
 
-export default App
+export default App;
